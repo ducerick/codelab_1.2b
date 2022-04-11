91e7083d51588addfb19254f01d74f7279142ef6
ducerick <buiminhduccsp@gmail.com>
2022-04-12 01:12:47 +0700

    Home work

--- app/src/main/java/duc/bm/hellotoast/MainActivity.java
+++ app/src/main/java/duc/bm/hellotoast/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
     private int mCount=0;
     private TextView mShowCount;
@@ -24,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
+
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
         }
+    }
+
+    public void setZero(View view) {
+        mShowCount.setText(Integer.toString(0));
     }
 }
